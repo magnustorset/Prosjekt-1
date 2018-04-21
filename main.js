c866d8a6aff7af3e7af8e0dd3e3e5ee60ce690a3
Magnus Torset <magnus.torset@gmail.com>
2018-04-21 22:11:43 +0200

    mac......

--- main.js
+++ main.js
@@ -1,57 +1,75 @@
-const  {electron, Menu} = require('electron');
-const app = require('electron').app;
-const BrowserWindow = require('electron').BrowserWindow;
+const  {app, BrowserWindow, Menu} = require('electron');
 
 let mainWindow;
 
-app.on('ready', () => {
-  mainWindow = new BrowserWindow({width: 1280, height: 800});
+const template = [
+  {
+    label: 'Edit', id: '2',
+    submenu: [
+      {role: 'undo'},
+      {role: 'redo'},
+      {type: 'separator'},
+      {role: 'cut'},
+      {role: 'copy'},
+      {role: 'paste'},
+      {role: 'pasteandmatchstyle'},
+      {role: 'delete'},
+      {role: 'selectall'}
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {role: 'toggledevtools'},
+      {type: 'separator'},
+      {role: 'resetzoom'},
+      {role: 'zoomin'},
+      {role: 'zoomout'},
+      {type: 'separator'},
+      {role: 'togglefullscreen'}
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      {role: 'minimize'},
+      {role: 'close'}
+    ]
+  }
+]
 
-  // Open Development Tools
-  mainWindow.openDevTools();
+if (process.platform === 'darwin') {
+  template.unshift({
+    label: app.getName(),
+    submenu: [
+      {role: 'quit'}
+    ]
+  })
+}
 
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-});
-
-app.on('window-all-closed', () => {
-  app.quit();
-});
-
-  const template = [
-    {
-      label: 'Edit',
-      submenu: [
-        {role: 'undo'},
-        {role: 'redo'},
-        {type: 'separator'},
-        {role: 'cut'},
-        {role: 'copy'},
-        {role: 'paste'},
-        {role: 'pasteandmatchstyle'},
-        {role: 'delete'},
-        {role: 'selectall'}
-      ]
-    },
-    {
-      label: 'View',
-      submenu: [
-        {role: 'toggledevtools'},
-        {type: 'separator'},
-        {role: 'resetzoom'},
-        {role: 'zoomin'},
-        {role: 'zoomout'},
-        {type: 'separator'},
-        {role: 'togglefullscreen'}
-      ]
-    },
-    {
-      role: 'window',
-      submenu: [
-        {role: 'minimize'},
-        {role: 'close'}
-      ]
-    }
-  ]
+if (process.platform === 'win32') {
+  template.unshift({
+    label: 'File',
+    submenu: [
+      {role: 'quit'}
+    ]
+  })
+}
 
   const menu = Menu.buildFromTemplate(template)
-  Menu.setApplicationMenu(menu)
+
+  app.on('ready', () => {
+    mainWindow = new BrowserWindow({width: 1280, height: 800});
+
+    // Open Development Tools
+    mainWindow.openDevTools();
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    Menu.setApplicationMenu(menu)
+
+  });
+
+  app.on('window-all-closed', () => {
+    app.quit();
+  });
