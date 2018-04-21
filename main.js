59f4bc06821586d1a65e16d00f8c9025b9c6b190
Magnus Torset <magnus.torset@gmail.com>
2018-04-21 22:15:45 +0200

    Kommenterte ut i tilfelle d ikkje funke

--- main.js
+++ main.js
@@ -1,62 +1,67 @@
-const  {app, BrowserWindow, Menu} = require('electron');
+// const  {app, BrowserWindow, Menu} = require('electron');
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
 
 let mainWindow;
 
-const template = [
-  {
-    label: 'Edit', id: '2',
-    submenu: [
-      {role: 'undo'},
-      {role: 'redo'},
-      {type: 'separator'},
-      {role: 'cut'},
-      {role: 'copy'},
-      {role: 'paste'},
-      {role: 'pasteandmatchstyle'},
-      {role: 'delete'},
-      {role: 'selectall'}
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
-      {role: 'toggledevtools'},
-      {type: 'separator'},
-      {role: 'resetzoom'},
-      {role: 'zoomin'},
-      {role: 'zoomout'},
-      {type: 'separator'},
-      {role: 'togglefullscreen'}
-    ]
-  },
-  {
-    role: 'window',
-    submenu: [
-      {role: 'minimize'},
-      {role: 'close'}
-    ]
-  }
-]
+// const template = [
+//   {
+//     label: 'Edit', id: '2',
+//     submenu: [
+//       {role: 'undo'},
+//       {role: 'redo'},
+//       {type: 'separator'},
+//       {role: 'cut'},
+//       {role: 'copy'},
+//       {role: 'paste'},
+//       {role: 'pasteandmatchstyle'},
+//       {role: 'delete'},
+//       {role: 'selectall'}
+//     ]
+//   },
+//   {
+//     label: 'View',
+//     submenu: [
+//       {role: 'toggledevtools'},
+//       {type: 'separator'},
+//       {role: 'resetzoom'},
+//       {role: 'zoomin'},
+//       {role: 'zoomout'},
+//       {type: 'separator'},
+//       {role: 'togglefullscreen'}
+//     ]
+//   },
+//   {
+//     role: 'window',
+//     submenu: [
+//       {role: 'minimize'},
+//       {role: 'close'}
+//     ]
+//   }
+// ]
+//
+// if (process.platform === 'darwin') {
+//   template.unshift({
+//     label: app.getName(),
+//     submenu: [
+//       {role: 'quit'}
+//     ]
+//   })
+// }
+//
+// if (process.platform === 'win32') {
+//   template.unshift({
+//     label: 'File',
+//     submenu: [
+//       {role: 'quit'}
+//     ]
+//   })
+// }
 
-if (process.platform === 'darwin') {
-  template.unshift({
-    label: app.getName(),
-    submenu: [
-      {role: 'quit'}
-    ]
-  })
-}
-
-if (process.platform === 'win32') {
-  template.unshift({
-    label: 'File',
-    submenu: [
-      {role: 'quit'}
-    ]
-  })
-}
-
-  const menu = Menu.buildFromTemplate(template)
+  // const menu = Menu.buildFromTemplate(template)
 
   app.on('ready', () => {
     mainWindow = new BrowserWindow({width: 1280, height: 800});
@@ -66,7 +71,7 @@ if (process.platform === 'win32') {
 
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-    Menu.setApplicationMenu(menu)
+    // Menu.setApplicationMenu(menu)
 
   });
 
