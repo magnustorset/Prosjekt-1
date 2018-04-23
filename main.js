3f426817699b15d47b429bec55042c105fd26408
magnustorset <magnus.torset@gmail.com>
2018-04-23 13:46:04 +0200

    Fjernet console ved start

--- main.js
+++ main.js
@@ -66,9 +66,6 @@ let mainWindow;
   app.on('ready', () => {
     mainWindow = new BrowserWindow({width: 1360, height: 800});
 
-    // Open Development Tools
-    mainWindow.openDevTools();
-
     mainWindow.loadURL('file://' + __dirname + '/index.html');
 
      Menu.setApplicationMenu(menu)
